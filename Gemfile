dd7de373a79306128d66d95ce5bc9b1a4f895e2f
Arthur Littmann <arthur.littmann@gmail.com>
2017-11-01 09:17:24 +0000

    initial setup

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
 
+gem 'nokogiri', '~> 1.6', '>= 1.6.8'
+
 group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,29 @@
+require 'nokogiri'
+require 'open-uri'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @products = load_desktop3dprinter
+  end
+
+  private
+
+  def load_desktop3dprinter
+    url = "http://www.desktop3dprinter.com/search/for/formlabs/page/"
+    pages = ["1/", "2/"]
+    products = []
+    pages.each do |page|
+      page_url = url + page
+      doc = Nokogiri::HTML(open(page_url))
+      doc.css('.jb_category_holder').each do |element|
+        name = element.css('jb_category_title').text.strip
+        price = element.css('jb_category_price').text.strip
+        website = 'desktop3dprinter'
+        products << {name: name, price: price, website: website}
+      end
+    end
+    return products
   end
 end
